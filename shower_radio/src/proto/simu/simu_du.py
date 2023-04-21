2ae923aba1b36ce88bd4c4fa1cf95f238a3f0d4b
luckyjim <jcolley@lpnhe.in2p3.fr>
2023-04-21 09:29:19 +0200

    improve doc

--- shower_radio/src/proto/simu/simu_du.py
+++ shower_radio/src/proto/simu/simu_du.py
@@ -4,13 +4,34 @@ Created on 20 avr. 2023
 @author: jcolley
 """
 
+import numpy as np
+import pprint
+import copy
+
+import matplotlib.pylab as plt
+
+
 from sradio.simu.du_resp import SimuDetectorUnitForEvent
 from sradio.io.shower.zhaires_master import ZhairesMaster
+import sradio.manage_log as mlg
+from sradio.basis.traces_event import Handling3dTracesOfEvent
+
 
 G_path_leff = "/home/jcolley/projet/grand_wk/data/model/detector"
 G_path_galaxy = ""
 G_path_rf_chain = ""
-G_path_simu = "/home/jcolley/projet/grand_wk/data/zhaires/Stshp_MZS_QGS204JET_Proton_0.21_56.7_90.0_5"
+G_path_simu = (
+    "/home/jcolley/projet/grand_wk/data/zhaires/Stshp_MZS_QGS204JET_Proton_0.21_56.7_90.0_5"
+)
+G_path_simu = (
+    "/home/jcolley/projet/grand_wk/bug/BugExample/Coarse2"
+)
+# specific logger definition for script because __mane__ is "__main__" !
+logger = mlg.get_logger_for_script(__file__)
+
+# define a handler for logger : standard only
+mlg.create_output_for_logger("debug", log_stdout=True)
+logger.info("test")
 
 def proto_simu_voc():
     dus = SimuDetectorUnitForEvent(G_path_leff)
@@ -18,8 +39,23 @@ def proto_simu_voc():
     data = event.get_object_3dtraces()
     d_info = event.get_simu_info()
     print(data)
-    print(d_info)
+    pprint.pprint(d_info)
     dus.set_data_efield(data)
+    shower = {}
+    shower["xmax"] = 1000*np.array([d_info["x_max"]["x"], d_info["x_max"]["y"], d_info["x_max"]["z"]])
+    dus.set_data_shower(shower)
+    dus.compute_du_all()
+    dus.o_ant3d.leff_ew.plot_leff_tan()
+    dus.o_ant3d.leff_sn.plot_leff_tan()
+    dus.o_ant3d.leff_up.plot_leff_tan()
+    print(data.traces[0])
+    print(dus.v_out[0])
+    data.plot_footprint_val_max()
+    out = copy.copy(data)
+    out.traces = dus.v_out
+    out.plot_footprint_val_max()
+
 
 if __name__ == "__main__":
     proto_simu_voc()
+    plt.show()
--- shower_radio/src/sradio/basis/coord.py
+++ shower_radio/src/sradio/basis/coord.py
@@ -8,37 +8,56 @@ see frame.py module to have frame definition and specific convention of axis and
 import numpy as np
 
 
-def frame_du_cart_to_sph(xyz_n):
-    """Return [DU] spherical angle in rad from cartesian
-            x to y  : azi_w = + 90
-            z to x  : d_zen = + 90
+#
+# [DU]
+#
 
-    :param xyz_n:
-    :type xyz_n:
+
+def cart_to_dir_du(xyz):
+    """Convert cartesian vector xyz to direction in [DU] frame
+
+    :param xyz: cartesian vector
+    :type xyz: float (3,)
+
+    :return: angle direction: azimuth, distance zenithal
+    :rtype: float (2,)
     """
-    # TODO: rewrite for vector input like (n,3)
-    azi_w = np.arctan2(xyz_n[1], xyz_n[0])
-    rho = np.sqrt(xyz_n[0] ** 2 + xyz_n[1] ** 2)
-    d_zen = np.arctan2(rho, xyz_n[2])
+    azi_w = np.arctan2(xyz[1], xyz[0])
+    rho = np.sqrt(xyz[0] ** 2 + xyz[1] ** 2)
+    d_zen = np.arctan2(rho, xyz[2])
     return np.array([azi_w, d_zen])
 
 
-def frame_du_cart_to_sph_dist(xyz_n):
-    """Return [DU] spherical angle in rad from cartesian
-            x to y  : azi_w = + 90
-            z to x  : d_zen = + 90
+def cart_to_sph_du(xyz):
+    """Convert cartesian vector xyz to spherical in [DU] frame
 
-    :param xyz_n:
-    :type xyz_n:
+    :param xyz: cartesian vector
+    :type xyz: float (3,)
+
+    :return: azimuth, distance zenithal, norm
+    :rtype: float (3,)
     """
     # TODO: rewrite for vector input like (n,3)
-    azi_w = np.arctan2(xyz_n[1], xyz_n[0])
-    rho_2 = xyz_n[0] ** 2 + xyz_n[1] ** 2
+    azi_w = np.arctan2(xyz[1], xyz[0])
+    rho_2 = xyz[0] ** 2 + xyz[1] ** 2
     rho = np.sqrt(rho_2)
-    d_zen = np.arctan2(rho, xyz_n[2])
-    return np.array([azi_w, d_zen, np.sqrt(rho_2 + xyz_n[2] ** 2)])
+    d_zen = np.arctan2(rho, xyz[2])
+    return np.array([azi_w, d_zen, np.sqrt(rho_2 + xyz[2] ** 2)])
 
 
-def frame_du_sph_to_cart(sph3):
-    """Return [DU]"""
-    pass
+def sph_to_cart_du(sph):
+    """Convert spherical vector xyz to cartesian in [DU] frame
+
+    :param sph: azimuth, distance zenithal, norm
+    :type sph: float (3,)
+
+    :return: cartesian vector
+    :rtype: float (3,)
+    """
+    c_a, s_a = np.cos(sph[0]), np.sin(sph[0])
+    c_dz, s_dz = np.cos(sph[1]), np.sin(sph[1])
+    xyz = np.empty_like(sph)
+    xyz[0] = s_dz * c_a
+    xyz[1] = s_dz * s_a
+    xyz[2] = c_dz
+    return sph[2] * xyz
