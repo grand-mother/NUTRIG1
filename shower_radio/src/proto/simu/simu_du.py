2e6c5e747811c183f049606807fe307609700e52
luckyjim <jcolley@lpnhe.in2p3.fr>
2023-04-25 11:47:21 +0200

    change name add model directory

--- shower_radio/src/proto/simu/simu_du.py
+++ shower_radio/src/proto/simu/simu_du.py
@@ -24,9 +24,7 @@ G_path_rf_chain = ""
 G_path_simu = (
     "/home/jcolley/projet/grand_wk/data/zhaires/Stshp_MZS_QGS204JET_Proton_0.21_56.7_90.0_5"
 )
-G_path_simu = (
-    "/home/jcolley/projet/grand_wk/bug/BugExample/Coarse2"
-)
+G_path_simu = "/home/jcolley/projet/grand_wk/bug/BugExample/Coarse2"
 # specific logger definition for script because __mane__ is "__main__" !
 logger = mlg.get_logger_for_script(__file__)
 
@@ -35,17 +33,19 @@ mlg.create_output_for_logger("debug", log_stdout=True)
 logger.info("test")
 f_out = "out_v_oc.asdf"
 
+
 def wiener_white_noise(measure, kernel, sigma):
     fft_m = fft.rfft(measure)
     fft_k = fft.rfft(kernel)
-    se_k = (fft_k*np.conj(fft_k)).real
-    se_s = (fft_m*np.conj(fft_m)).real- sigma
+    se_k = (fft_k * np.conj(fft_k)).real
+    se_s = (fft_m * np.conj(fft_m)).real - sigma
     idx_neg = np.where(se_s < 0)[0]
     se_s[idx_neg] = 0
-    wiener = (np.conj(fft_k)*se_s)/(se_k*se_s + sigma)
-    sol_w = fft.irfft(fft_m*wiener)
+    wiener = (np.conj(fft_k) * se_s) / (se_k * se_s + sigma)
+    sol_w = fft.irfft(fft_m * wiener)
     return sol_w, wiener, se_s
-    
+
+
 def proto_simu_voc():
     dus = SimuDetectorUnitForEvent(G_path_leff)
     event = ZhairesMaster(G_path_simu)
@@ -55,7 +55,9 @@ def proto_simu_voc():
     pprint.pprint(d_info)
     dus.set_data_efield(data)
     shower = {}
-    shower["xmax"] = 1000*np.array([d_info["x_max"]["x"], d_info["x_max"]["y"], d_info["x_max"]["z"]])
+    shower["xmax"] = 1000 * np.array(
+        [d_info["x_max"]["x"], d_info["x_max"]["y"], d_info["x_max"]["z"]]
+    )
     dus.set_data_shower(shower)
     dus.compute_du_all()
     dus.o_ant3d.leff_ew.plot_leff_tan()
@@ -68,11 +70,11 @@ def proto_simu_voc():
     out = copy.copy(data)
     out.traces = dus.v_out
     out.set_unit_axis("$\mu$V", "dir")
-    out.name += " V_oc" 
+    out.name += " V_oc"
     out.plot_footprint_val_max()
     fsrad.save_asdf_single_event(f_out, out, d_info)
-    
-    
+
+
 def proto_read():
     event, info = fsrad.load_asdf(f_out)
     pprint.pprint(info)
